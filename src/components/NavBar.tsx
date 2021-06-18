7280dd0f11cf4065da12d0b80eb2d777e0578665
ThomasTW935 <thomastw935@gmail.com>
2021-06-18 11:46:34 +0800

    Added middle part of the NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faPlay, faSearch, faStore, faUsers } from "@fortawesome/free-solid-svg-icons";
+
+const TABS = [
+  {
+    name: "home",
+    icon: faHome, 
+  },
+  {
+    name: "watch",
+    icon: faPlay, 
+  },
+  {
+    name: "market",
+    icon: faStore, 
+  },
+  {
+    name: "group",
+    icon: faUsers, 
+  },
+]
 
 export default function NavBar() {
+  const [activeTab, setActiveTab] = useState(TABS[0].name)
   return (
     <nav className="nav">
       {/* Left */}
       <div className="nav__left">
         <button className="logo">
-          <FontAwesomeIcon
-            className="logo__icon"
-            icon={faFacebook}
-          />
+          <FontAwesomeIcon className="logo__icon" icon={faFacebook} size="2x" />
         </button>
         <section className="search">
           <FontAwesomeIcon className="search__icon" icon={faSearch} />
@@ -25,7 +42,27 @@ export default function NavBar() {
       </div>
 
       {/* Middle */}
-      <div className="nav__middle"></div>
+      <div className="nav__middle">
+        {/* <button>
+          <FontAwesomeIcon className="icon" icon={faHome} size="lg" />
+        </button>
+        <button>
+          <FontAwesomeIcon className="icon" icon={faHome} size="lg" />
+        </button>
+        <button>
+          <FontAwesomeIcon className="icon" icon={faHome} size="lg" />
+        </button>
+        <button>
+          <FontAwesomeIcon className="icon" icon={faHome} size="lg" />
+        </button> */}
+        {
+          TABS.map(tab=>
+            <button className={activeTab === tab.name ? 'activeTab': ''} onClick={ ()=> { setActiveTab(tab.name) } }>
+              <FontAwesomeIcon className="icon" icon={tab.icon} size="lg" />
+            </button>
+          )
+        }
+      </div>
 
       {/* Right */}
       <div className="nav__right"></div>
