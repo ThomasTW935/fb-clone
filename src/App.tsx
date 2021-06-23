7228e7ff976603b528ed5b7ffb139cc4a75bfa70
ThomasTW935 <thomastw935@gmail.com>
2021-06-23 08:03:44 +0800

    Created AuthContext and AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import './styles/main.css'
-import Home from './pages/Home'
-import Marketplace from './pages/Marketplace'
-import Watch from './pages/Watch'
-import Group from './pages/Group'
+import React from "react";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./styles/main.css";
+import Home from "./pages/Home";
+import Marketplace from "./pages/Marketplace";
+import Watch from "./pages/Watch";
+import Group from "./pages/Group";
+import { useAuth } from "./context/AuthContext";
+import Login from "./components/authentication/Login";
 
 export default function App() {
+  const { currentUser } = useAuth();
   return (
     <div>
       <Router>
-        <NavBar/>
-        <Switch>
-          <Route component={Home} exact path='/'/>
-          <Route component={Watch} path='/watch'/>
-          <Route component={Marketplace} path='/marketplace'/>
-          <Route component={Group} path='/group'/>
-        </Switch>
+        {currentUser && (
+          <>
+            <NavBar />
+            <Switch>
+              <Route component={Home} exact path="/" />
+              <Route component={Watch} path="/watch" />
+              <Route component={Marketplace} path="/marketplace" />
+              <Route component={Group} path="/group" />
+            </Switch>
+          </>
+        )}
+        {!currentUser && (
+          <div>
+            <Login />
+          </div>
+        )}
       </Router>
     </div>
-  )
+  );
 }
-
