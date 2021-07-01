ad2450f0409c371ff618656ae8e26fe7b690e77e
ThomasTW935 <thomastw935@gmail.com>
2021-07-01 21:12:24 +0800

    Added Group to pages

---
+++ src/pages/group/Group.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: red;
+  width: 120px;
+  height: 20px;
+  border-radius: 5px;
+`
+export {Button}
---
+++ src/pages/group/Group.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Button} from './Group.styles'
+
+export default function Group() {
+  return (
+    <div>
+      <h1>Group</h1>
+      <Button>Hello</Button>
+    </div>
+  )
+}
