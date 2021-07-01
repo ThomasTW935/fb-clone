ec1991522eb04ae7596b4b9e18b6972a880837dd
ThomasTW935 <thomastw935@gmail.com>
2021-07-01 21:08:26 +0800

    Added test data

---
+++ src/data.ts
@@ -0,0 +1,28 @@
+import testImg from './assets/images/testImg.jpg'
+export const CONTACTS = [
+  {
+    name: "Maria Sophia P. Senillo",
+    profile_picture: testImg,
+    online: true,
+  },
+  {
+    name: "Czar Camua",
+    profile_picture: testImg,
+    online: false,
+  },
+  {
+    name: "Aries Dario",
+    profile_picture: testImg,
+    online: true,
+  },
+  {
+    name: "Karl Cris Bo",
+    profile_picture: testImg,
+    online: true,
+  },
+  {
+    name: "Allyssa Antonio",
+    profile_picture: testImg,
+    online: false,
+  },
+];
