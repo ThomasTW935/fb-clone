ef4e04f8a9b0460a656a5ca456754a9ff1ecd8ed
ThomasTW935 <thomastw935@gmail.com>
2021-07-01 21:12:09 +0800

    Added index in pages

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+import Home from "./home/Home";
+import Group from "./group/Group";
+import Watch from "./watch/Watch";
+import Marketplace from "./marketplace/Marketplace";
+
+export { Home, Group, Watch, Marketplace };
