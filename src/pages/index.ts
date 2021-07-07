269ce565cc42fff6a2eb2af53d40f8fc9a54f638
ThomasTW935 <thomastw935@gmail.com>
2021-07-08 05:02:33 +0800

    Added index.ts in pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
-import Home from "./home/Home";
-import Group from "./group/Group";
-import Watch from "./watch/Watch";
-import Marketplace from "./marketplace/Marketplace";
+import Home from './home'
+import Group from './group/Group'
+import Watch from './watch/Watch'
+import Marketplace from './marketplace/Marketplace'
 
-export { Home, Group, Watch, Marketplace };
+export { Home, Group, Watch, Marketplace }
