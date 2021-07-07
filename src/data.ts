65a7cef48d40886667f2c167a00c7ad1459a74da
ThomasTW935 <thomastw935@gmail.com>
2021-07-08 04:56:27 +0800

    Added Primary and Secondary Tabs

--- src/data.ts
+++ src/data.ts
@@ -1,28 +1,75 @@
+import { faFacebookMessenger } from '@fortawesome/free-brands-svg-icons'
+import {
+  faHome,
+  faPlay,
+  faStore,
+  faUsers,
+  faPlus,
+  faBell,
+  faCaretDown,
+} from '@fortawesome/free-solid-svg-icons'
 import testImg from './assets/images/testImg.jpg'
 export const CONTACTS = [
   {
-    name: "Maria Sophia P. Senillo",
+    name: 'Maria Sophia P. Senillo',
     profile_picture: testImg,
     online: true,
   },
   {
-    name: "Czar Camua",
+    name: 'Czar Camua',
     profile_picture: testImg,
     online: false,
   },
   {
-    name: "Aries Dario",
+    name: 'Aries Dario',
     profile_picture: testImg,
     online: true,
   },
   {
-    name: "Karl Cris Bo",
+    name: 'Karl Cris Bo',
     profile_picture: testImg,
     online: true,
   },
   {
-    name: "Allyssa Antonio",
+    name: 'Allyssa Antonio',
     profile_picture: testImg,
     online: false,
   },
-];
+]
+
+export const PRIMARY_TABS = [
+  {
+    name: 'home',
+    icon: faHome,
+  },
+  {
+    name: 'watch',
+    icon: faPlay,
+  },
+  {
+    name: 'marketplace',
+    icon: faStore,
+  },
+  {
+    name: 'group',
+    icon: faUsers,
+  },
+]
+export const SECONDARY_TABS = [
+  {
+    name: 'plus',
+    icon: faPlus,
+  },
+  {
+    name: 'messenger',
+    icon: faFacebookMessenger,
+  },
+  {
+    name: 'bell',
+    icon: faBell,
+  },
+  {
+    name: 'downArrow',
+    icon: faCaretDown,
+  },
+]
