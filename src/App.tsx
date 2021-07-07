84d08951180994b0546313e07eb50949d050120d
ThomasTW935 <thomastw935@gmail.com>
2021-07-08 05:03:19 +0800

    Fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import React, { useState } from "react";
-import NavBar from "../src/components/NavBar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./styles/main.css";
-import Home from "./pages/home/Home";
-import Marketplace from "./pages/marketplace/Marketplace";
-import Watch from "./pages/watch/Watch";
-import Group from "./pages/group/Group";
-import { useAuth } from "./auth/AuthContext";
-import Login from "../src/components/authentication/Login";
-import Signup from "../src/components/authentication/Signup";
+import React, { useState } from 'react'
+import NavBar from './components/NavBar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './styles/main.css'
+import { Home, Group, Watch, Marketplace } from './pages'
+import { useAuth } from './auth/AuthContext'
+import Login from '../src/components/authentication/Login'
+import Signup from '../src/components/authentication/Signup'
 
 export default function App() {
-  const { currentUser } = useAuth();
-  const [isLogin,setIsLogin] = useState(true)
+  const { currentUser } = useAuth()
+  const [isLogin, setIsLogin] = useState(true)
   return (
     <div>
       <Router>
@@ -20,19 +17,23 @@ export default function App() {
           <>
             <NavBar />
             <Switch>
-              <Route component={Home} exact path="/" />
-              <Route component={Watch} path="/watch" />
-              <Route component={Marketplace} path="/marketplace" />
-              <Route component={Group} path="/group" />
+              <Route component={Home} exact path='/' />
+              <Route component={Watch} path='/watch' />
+              <Route component={Marketplace} path='/marketplace' />
+              <Route component={Group} path='/group' />
             </Switch>
           </>
         )}
         {!currentUser && (
           <div className='authentication'>
-            {isLogin ? <Login setIsLogin={setIsLogin} /> : <Signup setIsLogin={setIsLogin}/> }
+            {isLogin ? (
+              <Login setIsLogin={setIsLogin} />
+            ) : (
+              <Signup setIsLogin={setIsLogin} />
+            )}
           </div>
         )}
       </Router>
     </div>
-  );
+  )
 }
