d43f9cb6a4b241aaa96a61984707e3ac029d33a4
ThomasTW935 <thomastw935@gmail.com>
2021-07-08 05:00:07 +0800

    Fix List style

--- src/core-ui/List.style.ts
+++ src/core-ui/List.style.ts
@@ -1,21 +1,20 @@
 import styled, { StyledComponentBase } from 'styled-components'
 
-interface IList extends StyledComponentBase<any, {}>{
-  Item ?: any
+interface IList extends StyledComponentBase<any, {}> {
+  Item?: any
 }
 
-const List:IList = styled.ul`
-  width:100%;
-`
+const List: IList = styled.ul``
 
 const ListItem = styled.li`
   display: flex;
   align-items: center;
-  padding: 0.5rem;
+  padding: 0.6rem;
   gap: 0.5rem;
   border-radius: 5px;
+  text-transform: capitalize;
   &:hover {
-    background: ${props=> props.theme.palette.common.grey.normal};
+    background: ${(props) => props.theme.palette.common.grey.normal};
   }
 `
 
