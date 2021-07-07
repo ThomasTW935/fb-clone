868899de4b8a1ea53a61c496bf514121d1638013
ThomasTW935 <thomastw935@gmail.com>
2021-07-08 05:03:05 +0800

    Added Global Style

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { AuthProvider } from './auth/AuthContext';
-import {ThemeProvider} from 'styled-components'
-import {defaultTheme as theme} from './core-ui/theme'
-import reportWebVitals from './reportWebVitals';
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { AuthProvider } from './auth/AuthContext'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme as theme } from './core-ui/theme'
+import reportWebVitals from './reportWebVitals'
+import GlobalStyle from './core-ui/reset'
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-     <AuthProvider>
-      <App />
-    </AuthProvider>
+      <AuthProvider>
+        <GlobalStyle />
+        <App />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
