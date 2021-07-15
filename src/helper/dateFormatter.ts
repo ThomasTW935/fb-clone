07e03eb413fb8e175c898b2862357796fcd993ae
ThomasTW935 <thomastw935@gmail.com>
2021-07-15 10:46:39 +0800

    added dateFormatter Helper function

---
+++ src/helper/dateFormatter.ts
@@ -0,0 +1,29 @@
+import moment, { MomentInput } from 'moment'
+
+const dateFormatter = (date: MomentInput) => {
+  const dateNow = moment()
+  moment.updateLocale('en', {
+    relativeTime: {
+      s: '%ds',
+      ss: '%ds',
+      m: '%dm',
+      mm: '%dm',
+      h: '%dh',
+      hh: '%dh',
+      d: '%dd',
+      dd: '%dd',
+    },
+  })
+
+  if (dateNow.diff(date, 'days') <= 7) {
+    return moment(date).fromNow(true)
+  } else if (moment().get('month') === moment(date).get('month')) {
+    return moment(date).format('MMMM D [at] h:mm A')
+  } else if (moment().get('year') === moment(date).get('year')) {
+    return moment(date).format('MMMM D')
+  } else {
+    return moment(date).format('MMMM D, YYYY')
+  }
+}
+
+export { dateFormatter }
