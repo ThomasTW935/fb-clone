dc516528bb2c8758426fc6e5fceb091eccb87b45
ThomasTW935 <thomastw935@gmail.com>
2021-07-17 00:14:30 +0800

    fix imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { AuthProvider } from './auth/AuthContext'
-import { ThemeProvider } from 'styled-components'
-import { defaultTheme as theme } from './core-ui/theme'
 import reportWebVitals from './reportWebVitals'
 import GlobalStyle from './core-ui/reset'
-
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme as theme } from './core-ui/theme'
+import { AuthProvider } from './auth/AuthContext'
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
