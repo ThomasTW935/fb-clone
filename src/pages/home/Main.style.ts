d91b32187d8801d4dddf08569ec8b645418bbc0c
ThomasTW935 <thomastw935@gmail.com>
2021-07-21 06:04:57 +0800

    Refactor List Style

--- src/pages/home/Main.style.ts
+++ src/pages/home/Main.style.ts
@@ -3,7 +3,7 @@ import styled, { StyledComponentBase } from 'styled-components'
 import { MainSideNav } from '../../core-ui/SideNav'
 import ImgCon from '../../core-ui/image/Profile-Rounded'
 import NewsFeed from './NewsFeed.style'
-import List from '../../core-ui/List.style'
+import baseListStyle from '../../core-ui/List.style'
 import baseMainStyle from '../../core-ui/Main'
 
 interface IMain extends StyledComponentBase<any, {}> {
@@ -27,6 +27,7 @@ const ContactHead = styled.div`
     gap: 0.5rem;
   }
 `
+const List = styled(baseListStyle)``
 
 Main.SideNav = MainSideNav
 Main.List = List
