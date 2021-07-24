5aa7e67c4ba99343eef85cec663bc367d5c5911d
ThomasTW935 <thomastw935@gmail.com>
2021-07-25 04:25:09 +0800

    Added Post selected

--- src/context/PostContext.ts
+++ src/context/PostContext.ts
@@ -1,23 +1,29 @@
 import { useContext, createContext, Dispatch } from 'react'
 import { IPost } from '../interfaces'
 
-export enum ACTIONS {
+export enum POST_ACTIONS {
   SET_POSTS = 'set-posts',
   ADD_POST = 'add-post',
+  UPDATE_POST = 'update-post',
+  SET_SELECTED_POST = 'set-selected-post',
 }
 
 type Reducer<TState, TAction> = (state: TState, action: TAction) => TState
 
 type TState = {
   posts: IPost[]
+  selectedPost: string
 }
 
 type TAction =
-  | { type: ACTIONS.SET_POSTS; payload: IPost[] }
-  | { type: ACTIONS.ADD_POST; payload: IPost }
+  | { type: POST_ACTIONS.SET_POSTS; payload: IPost[] }
+  | { type: POST_ACTIONS.ADD_POST; payload: IPost }
+  | { type: POST_ACTIONS.UPDATE_POST; payload: string }
+  | { type: POST_ACTIONS.SET_SELECTED_POST; payload: string }
 
 export const initialPostState: TState = {
   posts: [],
+  selectedPost: '',
 }
 
 type postContextType = {
@@ -40,10 +46,12 @@ export function usePostContext() {
 
 export const PostReducer: Reducer<TState, TAction> = (state, action) => {
   switch (action.type) {
-    case ACTIONS.SET_POSTS:
-      return { posts: action.payload }
-    case ACTIONS.ADD_POST:
-      return { posts: [action.payload, ...state.posts] }
+    case POST_ACTIONS.SET_POSTS:
+      return { ...state, posts: action.payload }
+    case POST_ACTIONS.ADD_POST:
+      return { ...state, posts: [action.payload, ...state.posts] }
+    case POST_ACTIONS.SET_SELECTED_POST:
+      return { ...state, selectedPost: action.payload }
     default:
       return state
   }
