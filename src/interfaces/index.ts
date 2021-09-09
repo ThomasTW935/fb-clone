f86bc82e2de46ede8c75404df07014145adbdb08
ThomasTW935 <thomastw935@gmail.com>
2021-09-10 07:07:37 +0800

    Added Reaction interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -4,12 +4,21 @@ interface IUser {
   first_name: string
   last_name: string
 }
+enum EReact {
+  Like = 'like',
+  Heart = 'heart',
+}
+interface IReaction {
+  user: IUser
+  react: EReact
+}
 interface IPost {
   privacy: string
   content: string
   _id: string
   createdAt: string
   user: IUser
+  reactions: IReaction[]
 }
 
 interface IUserData {
@@ -19,4 +28,4 @@ interface IUserData {
   password: string
 }
 
-export type { IUser, IPost, IUserData }
+export type { IUser, IPost, IUserData, EReact, IReaction }
