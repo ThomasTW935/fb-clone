1c3f78adf63e4908a0c49494816b45f95ce55a4f
ThomasTW935 <thomastw935@gmail.com>
2021-09-10 07:06:56 +0800

    Move footer to separate files

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react'
 import Con from './Post.style'
+import PostFooter from './PostFooter'
 import { IPost } from '../../interfaces'
 import testImg from '../../assets/images/testImg.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -7,11 +8,9 @@ import {
   faEllipsisH,
   faGlobeAsia,
   faLock,
-  faShare,
 } from '@fortawesome/free-solid-svg-icons'
 import { dateFormatter } from '../../helper/dateFormatter'
 import PostActions from './PostActions'
-import { faComment, faThumbsUp } from '@fortawesome/free-regular-svg-icons'
 
 interface IProps {
   post: IPost
@@ -63,32 +62,7 @@ export default function Post({ post }: IProps) {
         </Con.Actions>
       </Con.Head>
       <Con.Body>{post.content}</Con.Body>
-      <Con.Footer>
-        <Con.Footer.Reactions>
-          <div>
-            <FontAwesomeIcon icon={faThumbsUp} />
-            <span>25</span>
-          </div>
-          <div>
-            <span>25 Comments</span>
-            <span>25 Shares</span>
-          </div>
-        </Con.Footer.Reactions>
-        <Con.Footer.Actions>
-          <Con.Footer.Button>
-            <FontAwesomeIcon icon={faThumbsUp} color='black' />
-            <span>Like</span>
-          </Con.Footer.Button>
-          <Con.Footer.Button>
-            <FontAwesomeIcon icon={faComment} />
-            <span>Comment</span>
-          </Con.Footer.Button>
-          <Con.Footer.Button>
-            <FontAwesomeIcon icon={faShare} />
-            <span>Share</span>
-          </Con.Footer.Button>
-        </Con.Footer.Actions>
-      </Con.Footer>
+      <PostFooter post={post} />
     </Con>
   )
 }
